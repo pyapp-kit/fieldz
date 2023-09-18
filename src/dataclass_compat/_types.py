ae51e900b483031baaea2770b2abb92e6879c514
Talley Lambert <talley.lambert@gmail.com>
2023-09-18 08:28:34 -0400

    update annotation parsing

--- src/dataclass_compat/_types.py
+++ src/dataclass_compat/_types.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import dataclasses
 import enum
 import sys
+import warnings
 from typing import (
     TYPE_CHECKING,
     Annotated,
@@ -17,6 +18,9 @@ from typing import (
     get_origin,
 )
 
+if TYPE_CHECKING:
+    import builtins
+
 _T = TypeVar("_T")
 
 DC_KWARGS = {"frozen": True}
@@ -28,6 +32,17 @@ class _MISSING_TYPE(enum.Enum):
     MISSING = enum.auto()
 
 
+@dataclasses.dataclass(**DC_KWARGS)
+class DataclassParams:
+    init: bool = True
+    repr: bool = True
+    eq: bool = True
+    order: bool = False
+    unsafe_hash: bool = False
+    frozen: bool = False
+
+
+@dataclasses.dataclass(**DC_KWARGS)
 class Constraints:
     gt: int | float | None = None
     ge: int | float | None = None
@@ -40,9 +55,10 @@ class Constraints:
     decimal_places: int | None = None  # for decimal
     pattern: str | None = None
     deprecated: bool | None = None
-    timezone: bool | None = None
+    tz: bool | None = None
     # enum: list[Any] | None = None
     # const: Any | None = None
+    predicate: Callable[[Any], bool] | None = None
 
 
 @dataclasses.dataclass(**DC_KWARGS)
@@ -66,50 +82,127 @@ class Field(Generic[_T]):
     native_field: Any | None = dataclasses.field(default=None, compare=False)
     constraints: Constraints | None = None
 
+    # used internally
+    annotated_type: builtins.type[_T] | None = dataclasses.field(
+        default=None, repr=False, compare=False
+    )
 
-@dataclasses.dataclass(**DC_KWARGS)
-class DataclassParams:
-    init: bool = True
-    repr: bool = True
-    eq: bool = True
-    order: bool = False
-    unsafe_hash: bool = False
-    frozen: bool = False
+    @property
+    def is_annotated_type(self) -> bool:
+        """Whether the field is an Annotated type."""
+        return get_origin(self.type) is Annotated
+
+    def parse_annotated(self) -> Field[_T]:
+        """Extract info from Annotated type if present, and return new field.
+
+        If self.type is not an Annotated type, return self.
+        """
+        if not self.is_annotated_type:
+            return self
+
+        kwargs, constraints = _parse_annotated(self.type)
+
+        for key in ("default", "name"):
+            if (val := getattr(self, key)) not in (Field.MISSING, None) and kwargs.get(
+                key
+            ) not in (Field.MISSING, None):
+                warnings.warn(
+                    f"Cannot set {key!r} in both type annotation and field. Overriding "
+                    f"{key!r}={kwargs[key]!r} with {val!r}.",
+                    stacklevel=2,
+                )
+
+        if self.constraints is not None:
+            kwargs["constraints"] = dataclasses.replace(self.constraints, **constraints)
+        elif constraints:
+            kwargs["constraints"] = Constraints(**constraints)
+
+        return dataclasses.replace(self, **kwargs)
 
 
-def reduce_annotated(hint: Any) -> dict[str, Any]:
-    """Convert an Annotated type to a dict of UiField kwargs."""
-    # hint must be an Annotated[...] type
+def _parse_annotated(hint: Any) -> tuple[dict, dict]:
+    """Convert an Annotated type to a dict of Field kwargs."""
+    # hint should have been checked to be an Annotated[...] type
+    origin, *metadata = get_args(hint)
+    kwargs: dict[str, Any] = {"type": origin, "annotated_type": hint}
+    constraints: dict[str, Any] = {}
+
+    # deal with annotated_types
+    constraints.update(_parse_annotated_types(metadata))
+
+    # deal with msgspec
+    m_kwargs, m_constraints = _parse_msgspec_meta(metadata)
+    kwargs.update(m_kwargs)
+    constraints.update(m_constraints)
+
+    # TODO: support pydantic.fields.FieldInfo?
+    # TODO: support re.Pattern?
+    # TODO: support msgspec
+    return kwargs, constraints
+
+
+def _parse_annotated_types(metadata: list[Any]) -> dict[str, Any]:
+    """Extract constraints from annotated_types metadata."""
     if TYPE_CHECKING:
         import annotated_types
     else:
         annotated_types = sys.modules.get("annotated_types")
-    base_metas: list[annotated_types.BaseMetadata] = []
+        if annotated_types is None:
+            return {}
 
-    origin, *metadata = get_args(hint)
-    # TODO: support pydantic.fields.FieldInfo?
-    # TODO: support re.Pattern?
-    kwargs = {}
-    if annotated_types is not None:
-        for item in metadata:
-            # annotated_types >= 0.3.0 is supported
-            if isinstance(item, annotated_types.BaseMetadata):
-                base_metas.append(item)
-            elif isinstance(item, annotated_types.GroupedMetadata):
-                base_metas.extend(item)
+    a_kwargs = {}
+    for item in metadata:
+        # annotated_types >= 0.3.0 is supported
+        if isinstance(item, annotated_types.BaseMetadata):
+            a_kwargs.update(dataclasses.asdict(item))  # type: ignore
+        elif isinstance(item, annotated_types.GroupedMetadata):
+            for i in item:
+                a_kwargs.update(dataclasses.asdict(i))  # type: ignore
 
-    if base_metas:
-        _annotated_kwargs = {}
-        for i in base_metas:
-            _annotated_kwargs.update(dataclasses.asdict(i))  # type: ignore
-        if "max_exclusive" in _annotated_kwargs:
-            _annotated_kwargs["max_items"] = _annotated_kwargs.pop("max_exclusive") - 1
-        kwargs.update(_annotated_kwargs)
+    # annotated types calls the value of a Predicate "func"
+    if "func" in a_kwargs:
+        a_kwargs["predicate"] = a_kwargs.pop("func")
 
-    kwargs.update({"type": origin, "_original_annotation": hint})
-    return kwargs
+    # these were changed in v0.4.0
+    if "min_inclusive" in a_kwargs:  # pragma: no cover
+        a_kwargs["min_length"] = a_kwargs.pop("min_inclusive")
+    if "max_exclusive" in a_kwargs:  # pragma: no cover
+        a_kwargs["max_length"] = a_kwargs.pop("max_exclusive") - 1
+    return a_kwargs
 
 
-def _is_annotated_type(hint: Any) -> bool:
-    """Return True the field is an Annotated type."""
-    return get_origin(hint) is Annotated
+# At the moment, all of our constraint names match msgspec.Meta attributes
+# (we are a superset of msgspec.Meta)
+CONSTRAINT_NAMES = {f.name for f in dataclasses.fields(Constraints)}
+FIELD_NAMES = {f.name for f in dataclasses.fields(Field)}
+
+
+def _parse_msgspec_meta(metadata: list[Any]) -> tuple[dict, dict]:
+    """Extract constraints from msgspec.Meta metadata."""
+    if TYPE_CHECKING:
+        import msgspec
+    else:
+        msgspec = sys.modules.get("msgspec")
+        if msgspec is None:
+            return {}, {}
+
+    field_kwargs = {}
+    constraints = {}
+    for item in metadata:
+        if isinstance(item, msgspec.Meta):
+            constraints.update(
+                {
+                    k: val
+                    for k in CONSTRAINT_NAMES
+                    if (val := getattr(item, k, None)) is not None
+                }
+            )
+            field_kwargs.update(
+                {
+                    k: val
+                    for k in FIELD_NAMES
+                    if (val := getattr(item, k, None)) is not None
+                }
+            )
+
+    return field_kwargs, constraints
