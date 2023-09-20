b348dfc125006eb62e3c3753b5af7d94e28e31fa
Talley Lambert <talley.lambert@gmail.com>
2023-09-20 07:22:14 -0400

    more

--- src/fieldz/adapters/__init__.py
+++ src/fieldz/adapters/__init__.py
@@ -1,7 +1,4 @@
 """Adapter modules for fieldz."""
-
-from typing import Tuple
-
 from . import (
     _attrs,
     _dataclasses,
@@ -13,15 +10,13 @@ from . import (
 )
 from .protocol import Adapter
 
-# Order matters here. The first adapter to return True for is_instance will be used.
-ADAPTER_MODULES: Tuple[Adapter, ...] = (
-    _pydantic,
-    _attrs,
-    _msgspec,
-    _dataclassy,
-    _dataclasses,
-    _named_tuple,
-    _typed_dict,
-)
-
-__all__ = ["ADAPTER_MODULES", "Adapter"]
+__all__ = [
+    "Adapter",
+    "_attrs",
+    "_dataclasses",
+    "_dataclassy",
+    "_msgspec",
+    "_named_tuple",
+    "_pydantic",
+    "_typed_dict",
+]
